622c54291284fa12d6f915b4d2bcd2fb485846a2
Anderson Danilo <contato@andersondanilo.com>
2020-09-04 21:34:11 -0300

    circle ci config

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,7 +3,7 @@ require "../src/glassy-mongo-odm"
 
 def make_connection
   db_name = "default_db"
-  conn = Glassy::MongoODM::Connection.new("mongodb://mongo", db_name)
+  conn = Glassy::MongoODM::Connection.new("mongodb://#{ENV["MONGO_HOST"]? || "mongo"}", db_name)
   conn.client[db_name].drop
   conn
 end
